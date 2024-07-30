53cfe5729912e78a00b9a3c7efb62b950c26f46e
qodirov-a-0110 <qodirovabdumajid03@gmail.com>
2024-07-30 15:56:59 +0500

    .

---
+++ index.js
@@ -0,0 +1,176 @@
+
+// 1-misol
+// function kv(arg) {
+//     let res = arg **2;
+//     return res;
+// }
+// let kvd = kv(2);
+// console.log(kvd);
+
+// 2-misol
+// function kv(arg) {
+//     let res = arg.length;
+//     return res;
+// }
+// let kvd = kv('salom');
+// console.log(kvd);
+
+// 3-misol
+// function kv(arg, arg2) {
+//     let res = arg+arg2;
+//     return res;
+// }
+// let kvd = kv(2,3);
+// console.log(kvd);
+
+// 4-misol
+// function kv(arg, arg2) {
+//     let res;
+//     if (arg> arg2) {
+//         res = arg;
+//     }else{
+//         res = arg2
+//     }
+//     return res;
+// }
+// let kvd = kv(2,3);
+// console.log(kvd);
+
+// 5-misol
+// function kv(arg) {
+//     let res = arg.revers
+//     return res;
+// }
+// let kvd = kv('salom');
+// console.log(kvd);
+
+// 6-misol
+// function a (arg1) {
+//     let res ;
+//     if (arg1>0) {
+//         res = 'musbat'
+//     }else{
+//         res = 'manfiy'
+//     }
+//     return res;
+// }
+// let  b = a(40);
+// console.log(b);
+
+// 7-misol
+// function name(arg) {
+//     let res = arg.toUpperCase();
+//     return res;
+// }
+// let sum = name('salom')
+// console.log(sum);
+
+// 8-misol
+// function a (arg1) {
+//     let res ;
+//     if (arg1 % 2 == 0) {
+//         res = 'juft'
+//     }else{
+//         res = 'toq'
+//     }
+//     return res;
+// }
+// let  b = a(40);
+// console.log(b);
+
+// 9-misol
+// function name(arg) {
+//     let res = arg.length;
+//     return res;
+// }
+// let sum = name('salom')
+// console.log(sum);
+
+// 10-misol
+// function sum (arg1, arg2) {
+//     let res = arg1 * arg2;
+//     return res;
+// }
+// let sum1 = sum(5,8);
+// console.log(sum1);
+
+// 11-misol
+// function name(arg) {
+//     let res = arg.toLowerCase();
+//     return res;
+// }
+// let sum = name('SALOM')
+// console.log(sum);
+
+// 12-misol
+// function str(a) {
+//   let m = a.charAt(0).toUpperCase();
+//   let r = a.charAt();
+
+//   return m;
+// }
+
+// let a = "salom";
+// let res = str(a);
+// console.log(res);
+
+// 13-misol
+// function res (arg) {
+//     let sum ;
+//     for (const iterator of arg) {
+//         sum = iterator * iterator
+//     }
+//     return sum;
+// }
+// let a = res(5);
+// console.log(a);
+
+// 14-tushunmadim
+
+// 15-misol
+// function Numbers(arg, arg1) {
+//     return arg / arg1;
+// }
+
+// let a  = 10;
+// let b = 2;
+// let res = Numbers(a, b);
+// console.log(res);
+
+// b = 0;
+// res = Numbers(a, b);
+
+// ===== keyingi mavzu ======
+// 1-masala
+// **Massivdagi barcha elementlarning yig'indisini hisoblaydigan funksiya yozing.**
+// function elements(arg) {
+//     let sum = 0;
+//     for (const iterator of arg) {
+//         sum +=iterator
+//     }
+//     return sum;
+// }
+// let res = elements([1,2,4,8,7,9]);
+// console.log(res);
+
+// 2-misol
+//  **Massivdagi eng katta elementni topadigan funksiya yozing.**
+// function bigelements(arg) {
+//     let res= 0 ;
+//     for (const iterator of arg) {
+//         if (iterator > res) {
+//             res = iterator
+//         }else{
+//             res = res;
+//         }
+//     }
+//     return res;
+// }
+// let a = bigelements([1,4,7,8,9,6,3]);
+// console.log(a);
+
+// 3-misol
+//  **Massivni teskari tartibda qaytaradigan funksiya yozing.**
+function revers() {
+    
+}
